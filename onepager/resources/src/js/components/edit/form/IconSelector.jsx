fdf1583152147d0ffcc1791ec884c51786d6b743
Nadim Tuhin <nadimtuhin@gmail.com>
2015-06-19 02:43:11 +0600

    [Refactor] refactored media, icon and image components to be faster

--- onepager/resources/src/js/components/edit/form/IconSelector.jsx
+++ onepager/resources/src/js/components/edit/form/IconSelector.jsx
@@ -1,47 +1,49 @@
-const $ = jQuery;
-const React = require('react');
-const ContainedSelectorMixin = require("../../../mixins/ContainedSelectorMixin");
-const ReactComponentWithPureRenderMixin = require('react/lib/ReactComponentWithPureRenderMixin');
+const $         = jQuery;
+const React     = require('react');
+const PureMixin = require('react/lib/ReactComponentWithPureRenderMixin');
+const dom       = React.domDOMNode;
 
 let IconSelector = React.createClass({
-  mixins: [ContainedSelectorMixin, ReactComponentWithPureRenderMixin],
+  mixins: [PureMixin],
 
   getValue(){
-    return React.findDOMNode(this.refs.input).value;
+    return dom(this.refs.input).value;
   },
 
   componentDidMount() {
-    let buttonEl = this.getSelector(".icon-selector-button");
-    let inputEl = this.getSelector(".icon-input");
+    let buttonEl  = dom(this.refs.select);
+    let inputEl   = dom(this.refs.input);
 
     $(buttonEl).iconSelector({input: inputEl});
     $(inputEl).on("icon:inserted", this.props.onChange);
   },
 
   componentWillUnmount(){
-    let buttonEl = this.getSelector(".icon-selector-button");
-    let inputEl = this.getSelector(".icon-input");
-
-    $(buttonEl).unbind();
-    $(inputEl).unbind();
+    $(dom(this.refs.select)).unbind();
+    $(dom(this.refs.input)).unbind();
   },
 
-
   render() {
-      return (
-        <div ref="container" className="icon-selector">
-          <div className="form-group">
-            <label>{this.props.label}</label>
-            <div className="input-group">
-              <input {...this.props} type="text" className={"form-control icon-input "+this.props.className} ref="input"/>
-              <span className="input-group-btn">
-                <button className="btn btn-primary icon-selector-button" ref="icon-btn" type="button"><span className="fa fa-diamond"></span> Icon</button>
-              </span>
-            </div>
-            <div className="media-preview"></div>
-          </div>
+    let classes = "form-control icon-input "+this.props.className;
+
+    return (
+      <div className="form-group">
+        <label>{this.props.label}</label>
+        
+        <div className="input-group">
+          <input {...this.props} type="text" className={classes} ref="input"/>
+        
+          <span className="input-group-btn">
+            <button className="btn btn-primary" ref="select" type="button">
+              <span className="fa fa-diamond"></span> Icon
+            </button>
+          </span>
+
         </div>
-      );
+        
+        <div className="media-preview"></div>
+      </div>
+    );
   }
 });
 
