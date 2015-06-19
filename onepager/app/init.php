2c3cb7e5c167353170398bec696370a0a1ffc749
Parvez Akther <parvez@themexpert.com>
2015-06-19 14:35:59 +0600

    [Feature] SVG image upload support

--- onepager/app/init.php
+++ onepager/app/init.php
@@ -48,3 +48,9 @@ add_filter( 'the_content', function ( $content ) {
 	}
 
 } );
+
+// Add svg upload support
+add_filter('upload_mimes', function($mimes){
+	$mimes['svg'] = 'image/svg+xml';
+  	return $mimes;
+});
