6ad030fef105914eda7696de04a85cf9a770a1b2
Parvez Akther <parvez@themexpert.com>
2015-06-25 17:06:45 +0600

    [Blocks] Header 2 done and others block small fix

---
+++ onepager/blocks/headers/header-2/style.php
@@ -0,0 +1,50 @@
+#<?php echo $id; ?>{
+	background-image : url(<?php echo $styles['slider_bg']?>);
+	background-size : cover;
+	background-repeat : no-repeat;
+}
+
+
+#<?php echo $id; ?> .navbar.affix{
+	background-color : <?php echo $styles['nav_bg']?>
+}
+#<?php echo $id ?> .navbar-nav > li > a{
+	color : <?php echo $styles['link_color']; ?>;
+}
+#<?php echo $id ?> .navbar-toggle .icon-bar{
+	background-color : <?php echo $styles['link_color']; ?>;
+}
+
+#<?php echo $id ?> .navbar-nav > li:hover > a,
+#<?php echo $id ?> .navbar-nav > li.active > a{
+	color : <?php echo $styles['link_hover_color']; ?>;
+}
+
+#<?php echo $id ?> .navbar-btn{
+	background : <?php echo $styles['cta_bg']; ?>;
+	color : <?php echo $styles['cta_color']; ?>;
+}
+#<?php echo $id ?> .navbar.affix .navbar-btn{
+	background : <?php echo $styles['cta_color']; ?>;
+	color : <?php echo $styles['cta_bg']; ?>;
+}
+
+#<?php echo $id ?> .navbar.affix .navbar-btn:hover{
+	box-shadow: 0 0 4px #444;
+}
+
+
+
+#slide-<?php echo $id; ?> .btn{
+	background : <?php echo $styles['cta_bg']; ?>;
+	color : <?php echo $styles['cta_color']; ?>;
+}
+
+
+
+@media (min-width: 992px) { 
+	#slide-<?php echo $id; ?>,
+	#slide-<?php echo $id; ?> .item{
+		height : <?php echo $settings['slider_height']?>px;	
+	}
+}
