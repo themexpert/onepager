a68ee59fcb83e128806cb48e4543c794f7937311
Nadim Tuhin <nadimtuhin@gmail.com>
2015-06-25 15:54:53 +0600

    [refactor] moved css from assets

--- onepager/app/assets.php
+++ onepager/app/assets.php
@@ -33,25 +33,21 @@ function enqueueOnepagerAssets() {
 	$q->style( 'tx-animatecss', asset( 'assets/css/animate.css' ) );
 	$q->style( 'tx-fontawesome', asset( 'assets/css/font-awesome.css' ) );
 	$q->style( 'op-blocks', asset( 'assets/css/blocks.css' ) );
+	
 	$q->script( 'tx-bootstrap', asset( 'assets/js/bootstrap.js' ), [ 'jquery' ] );
 	$q->script( 'tx-wow', asset('assets/js/wow.js'), array( 'jquery' ) );
 	$q->script( 'tx-nicescroll', asset('assets/js/jquery.nicescroll.js'), array( 'jquery' ) );
 
 	$q->style( 'tx-flexbox', asset( 'assets/css/flex.css' ) );
-	$q->style( 'lithium', asset( 'assets/css/lithium.css' ) );
 
 	if ( onepager()->content()->isLiveMode() ) {
 		if ( function_exists( 'wp_enqueue_media' ) ) {
 			wp_enqueue_media();
 		}
 
-		$q->style( 'tx-colorpicker', asset( 'bower_components/mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.css' ) );
-		$q->style( 'tx-iconselector', asset( 'assets/css/icon-selector.min.css' ) );
-		$q->style( 'tx-sweetalert', asset( 'assets/css/sweetalert.css' ) );
-		$q->style( 'tx-toastr', asset( 'assets/css/toastr.css' ) );
-
 		$q->script( 'tx-iconselector', asset( 'assets/js/icon-selector.min.js' ), [ 'jquery' ] );
 		$q->script( 'tx-colorpicker', asset( 'assets/js/bootstrap-colorpicker.js' ), [ 'jquery' ] );
+		$q->script( 'tx-bootstrap-switch', asset( 'assets/js/bootstrap-switch.js' ), [ 'jquery' ] );
 		$q->script( 'tx-toastr', asset( 'assets/js/toastr.js' ), [ 'jquery' ] );
 
 		$q->script( 'onepager', asset('assets/app.bundle.js'), ['jquery']);
@@ -73,11 +69,6 @@ function enqueueOnepagerAdminAssets(){
 	$q->script( 'tx-bootstrap', asset( 'assets/js/bootstrap.js' ), [ 'jquery' ] );
 
 	$q->script( 'admin-bundle', asset('assets/admin.bundle.js'), ['jquery']);
-
-	$q->style( 'tx-colorpicker', asset( 'bower_components/mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.css' ) );
-	$q->style( 'tx-iconselector', asset( 'assets/css/icon-selector.min.css' ) );
-	$q->style( 'tx-sweetalert', asset( 'assets/css/sweetalert.css' ) );
-	$q->style( 'tx-toastr', asset( 'assets/css/toastr.css' ) );
 
 	$q->script( 'tx-iconselector', asset( 'assets/js/icon-selector.min.js' ), [ 'jquery' ] );
 	$q->script( 'tx-colorpicker', asset( 'assets/js/bootstrap-colorpicker.js' ), [ 'jquery' ] );
