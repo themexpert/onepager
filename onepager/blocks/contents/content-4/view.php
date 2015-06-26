01080c763b321f595a1cac9762fa072eaa1495d7
Parvez Akther <parvez@themexpert.com>
2015-06-26 23:34:15 +0600

    content 4 fix

--- onepager/blocks/contents/content-4/view.php
+++ onepager/blocks/contents/content-4/view.php
@@ -1,29 +1,31 @@
 <?php
-	$animation = ($settings['animation']) ? $settings['animation'] : '';
+	$aniamtion = ($settings['animation']) ? $settings['animation'] : '';
 ?>
 
 <section id="<?php echo $id;?>" class="op-section content-4">
 	<div class="container">
 		<div class="row">
 			<article class="flex flex-middle">
+
 				<div class="col-sm-9">
 					<div class="pad-right-big">
 						<!-- Title -->
 						<?php if($contents['title']): ?>
-							<h1 class="section-title <?php echo $settings['title_transformation']?> <?php echo $settings['title_size']?> wow <?php echo $animation; ?>"><?php echo $contents['title']?></h1>
+							<h1 class="section-title <?php echo $settings['title_transformation']?> <?php echo $settings['title_size']?> wow <?php echo $animation;?>"><?php echo $contents['title']?></h1>
 						<?php endif; ?>
 						<!-- Description -->
 						<?php if($contents['description']): ?>
-							<p class="desc wow <?php echo $animation; ?>"><?php echo $contents['description']?></p>
+							<p class="desc"><?php echo $contents['description']?></p>
 						<?php endif; ?>
 					</div>
 				</div>
 				
 				<div class="col-sm-3">	
 					<?php if( $contents['link']): ?>
-						<a class="btn btn-primary btn-lg wow <?php echo $animation; ?>" href="<?php echo $contents['link']?>"><?php echo $contents['link_text']; ?></a>
+						<a class="btn btn-primary btn-lg pull-right" href="<?php echo $contents['link']?>"><?php echo $contents['link_text']; ?></a>
 					<?php endif; ?>
 				</div>
+				
 			</article>
 		</div>
 	</div>
