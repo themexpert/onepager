48746773ceed949cea4e5a4af52d30a7b2b78a09
Nadim Tuhin <nadimtuhin@gmail.com>
2015-06-26 11:43:22 +0600

    [refactor] added comments to bootstrap.php

--- onepager/app/bootstrap.php
+++ onepager/app/bootstrap.php
@@ -4,12 +4,15 @@
 require( __DIR__ . "/../vendor/autoload.php" );
 require( __DIR__ . "/../src/functions.php" );
 
+//if we are on debug mode run
+//whoops to give nice error messages
 if(WP_DEBUG){
 	//add whoops
 	$whoops = new \Whoops\Run;
 	$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 	$whoops->register();
 }
+
 
 //create onepager instance
 use Pimple\Container;
@@ -20,6 +23,7 @@ $onepager = new Onepager(
 	new WordPress( new Container, ONEPAGER_PATH, ONEPAGER_URL ),
 	new Container
 );
+
 
 //require assets and more
 // require( __DIR__ . "/metabox.php" );
