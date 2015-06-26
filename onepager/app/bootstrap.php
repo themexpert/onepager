208562cc2923bcce5000ee7e0385da06d59da38c
Nadim Tuhin <nadimtuhin@gmail.com>
2015-06-26 11:26:05 +0600

    [php] added whoops package

--- onepager/app/bootstrap.php
+++ onepager/app/bootstrap.php
@@ -4,10 +4,12 @@
 require( __DIR__ . "/../vendor/autoload.php" );
 require( __DIR__ . "/../src/functions.php" );
 
-//add whoops
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();
+if(WP_DEBUG){
+	//add whoops
+	$whoops = new \Whoops\Run;
+	$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+	$whoops->register();
+}
 
 //create onepager instance
 use Pimple\Container;
