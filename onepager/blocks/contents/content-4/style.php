4d5bce6d128ab03d12b27630008061285aa421f9
Parvez Akther <parvez@themexpert.com>
2015-06-26 18:03:17 +0600

    Content 4 block done

---
+++ onepager/blocks/contents/content-4/style.php
@@ -0,0 +1,15 @@
+#<?php echo $id ?>{
+	background-color : <?php echo $styles['bg_color'] ?>;
+	color : <?php echo $styles['text_color']?>;
+}
+#<?php echo $id ?> .section-title{
+	color : <?php echo $styles['title_color']?>;
+}
+#<?php echo $id ?> .desc{
+	color : <?php echo $styles['text_color']?>;
+}
+#<?php echo $id ?> .btn{
+	background: transparent;
+	border: 3px solid <?php echo $styles['button_border_color']?>;
+	color : <?php echo $styles['button_text_color']?>;
+}
---
+++ onepager/blocks/contents/content-4/view.php
@@ -0,0 +1,30 @@
+<?php
+	$animation = ($settings['animation']) ? $settings['animation'] : '';
+?>
+
+<section id="<?php echo $id;?>" class="op-section content-4">
+	<div class="container">
+		<div class="row">
+			<article class="flex flex-middle">
+				<div class="col-sm-9">
+					<div class="pad-right-big">
+						<!-- Title -->
+						<?php if($contents['title']): ?>
+							<h1 class="section-title <?php echo $settings['title_transformation']?> <?php echo $settings['title_size']?> wow <?php echo $animation; ?>"><?php echo $contents['title']?></h1>
+						<?php endif; ?>
+						<!-- Description -->
+						<?php if($contents['description']): ?>
+							<p class="desc wow <?php echo $animation; ?>"><?php echo $contents['description']?></p>
+						<?php endif; ?>
+					</div>
+				</div>
+				
+				<div class="col-sm-3">	
+					<?php if( $contents['link']): ?>
+						<a class="btn btn-primary btn-lg wow <?php echo $animation; ?>" href="<?php echo $contents['link']?>"><?php echo $contents['link_text']; ?></a>
+					<?php endif; ?>
+				</div>
+			</article>
+		</div>
+	</div>
+</section>
