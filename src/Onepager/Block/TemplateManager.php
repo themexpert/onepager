01e2bb4496e9c672873d2cbde9a31601db407f59
Nadim Tuhin <nadimtuhin@gmail.com>
2015-07-09 21:04:01 +0600

    [refactor]

--- src/Onepager/Block/TemplateManager.php
+++ src/Onepager/Block/TemplateManager.php
@@ -3,44 +3,49 @@
 use ThemeXpert\FileSystem\FileSystem as FS;
 use ThemeXpert\Onepager\Block\Transformers\ConfigTransformer;
 
-class TemplateManager {
-	protected $templates;
-	protected $paths;
+class TemplateManager
+{
+    protected $templates;
+    protected $paths;
 
-	public function loadAllFromPath( $path, $url ) {
-		$this->paths[] = array(
-			"path" => $path,
-			"url"  => $url
-		);
-	}
+    public function loadAllFromPath($path, $url)
+    {
+        $this->paths[] = array(
+            "path" => $path,
+            "url" => $url
+        );
+    }
 
-	public function add( $file, $url ) {
-		$config = json_decode(file_get_contents( $file ), true);
-		if(!array_key_exists('screenshot', $config)){
-			$filename = basename($file);
-			$imagename = str_replace('.json', '.png', $filename);
-			$config['screenshot'] = trailingslashit( $url ) . $imagename;
-			$config['id'] = $filename;
-		}
+    public function add($file, $url)
+    {
+        $config = json_decode(file_get_contents($file), true);
+        if (!array_key_exists('screenshot', $config)) {
+            $filename = basename($file);
+            $imagename = str_replace('.json', '.png', $filename);
+            $config['screenshot'] = trailingslashit($url) . $imagename;
+            $config['id'] = $filename;
+        }
 
-		$this->templates[] = $config;
-	}
+        $this->templates[] = $config;
+    }
 
-	public function loadAll(){
-		foreach($this->paths as $path){
-			$files = array_filter(FS::files( $path['path'] ), function($file){
-				return substr($file, -4, strrpos($file, '.json')) === "json";
-			});
+    public function loadAll()
+    {
+        foreach ($this->paths as $path) {
+            $files = array_filter(FS::files($path['path']), function ($file) {
+                return substr($file, -4, strrpos($file, '.json')) === "json";
+            });
 
-			foreach ( $files as $file ) {
-				$config_file = untrailingslashit($path['path']) . DIRECTORY_SEPARATOR . $file;
-	      $this->add( $config_file, $path['url'] );
-			}
-		}
-	}
+            foreach ($files as $file) {
+                $config_file = untrailingslashit($path['path']) . DIRECTORY_SEPARATOR . $file;
+                $this->add($config_file, $path['url']);
+            }
+        }
+    }
 
-	public function all() {
-		$this->loadAll();
-		return $this->templates;
-	}
+    public function all()
+    {
+        $this->loadAll();
+        return $this->templates;
+    }
 }
