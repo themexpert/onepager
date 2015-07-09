462a3088d78e56af73d9858b68c41e43416e78e2
Nadim Tuhin <nadimtuhin@gmail.com>
2015-07-09 18:05:05 +0600

    [php] added singleton view instance

--- src/View/View.php
+++ src/View/View.php
@@ -1,9 +1,19 @@
 <?php namespace ThemeXpert\View;
+use ThemeXpert\View\Engines\PhpEngine;
 
 use ThemeXpert\View\Engines\EngineInterface;
 
 class View {
 	protected $compilerEngine;
+	protected static $instance;
+
+	public static function getInstance(){
+		if(!self::$instance){
+			self::$instance = new self(new PhpEngine);
+		}
+
+		return self::$instance;
+	}
 
 	public function __construct( EngineInterface $compilerEngine ) {
 		$this->compilerEngine = $compilerEngine;
