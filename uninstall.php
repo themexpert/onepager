9b9a99c88846bd0c5fcf574d9c486210738ccd25
Nadim Tuhin <nadimtuhin@gmail.com>
2015-07-09 18:16:22 +0600

    [php] added uninstall file

---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: na
+ * Date: 7/9/15
+ * Time: 4:45 AM
+ */
+
+if( !defined( 'ABSPATH' ) && !defined( 'WP_UNINSTALL_PLUGIN' ) )
+    exit();
+
+//do cleanup
