88b39975ae33df501bf5b06bf80e68544399be8d
Parvez Akther <parvez@themexpert.com>
2015-07-13 14:25:36 +0600

    Working on settings API and its wrapper

--- app/settings.php
+++ app/settings.php
@@ -1,15 +1,23 @@
 <?php
 // this file should be in the theme
 
-onepager()->optionsPanel("onepager")
-  ->tab("general", "General")
-  ->add(
-    array("name" => "title"),
-    array("name" => "type")
-  )
-  ->tab("options", "Options")
-  ->add(
-    array("name" => "title 1"),
-    array("name" => "type 1")
-  );
+Onepager::getOptionPanel()->tab('general', 'Generals')
+        ->add(
+          array('name' => 'favicon', 'type' => 'image'),
+          array(
+            'name'  => 'google_analytics', 
+            'type'  => 'textarea',
+            'label' => 'Google Analytics',
+            'placeholder' => 'Paste your code here'
+          ),
+          array(
+            'name' => 'full_screen',
+            'type' => 'switch',
+            'label' => 'Full Screen Page',
+          )
+        );
 
+
+add_action('wp_head', function(){
+  echo Onepager::getOption('google_analytics');
+});
