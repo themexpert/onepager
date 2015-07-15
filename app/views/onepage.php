9d6611b72ace8b27ac806776f4a6a4bfcfb3da55
Parvez Akther <parvez@themexpert.com>
2015-07-15 16:02:28 +0600

    removed duplicate meta tag

--- app/views/onepage.php
+++ app/views/onepage.php
@@ -11,7 +11,6 @@ Template Name: Onepage
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<meta name="viewport" content="width=device-width, initial-scale=1" />
 <?php wp_head(); ?>
 </head>
 
