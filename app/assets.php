ee5dafee31758daa84fbe731b05b56627e87fa28
Nadim Tuhin <nadimtuhin@gmail.com>
2015-07-16 18:28:21 +0600

    support for twentythirteen, twentyfourteen and twentyfifteen

--- app/assets.php
+++ app/assets.php
@@ -1,99 +1,114 @@
 <?php
 
-function getOnepagerData( $pageId ) {
-	$onepager = onepager();
+function getOnepagerData($pageId)
+{
+  $onepager = onepager();
 
-	$ajaxUrl   = $onepager->api()->getAjaxUrl();
-	$nav_arr   = $onepager->content()->getMenus();
-	$cat_arr   = $onepager->content()->getCategories();
-	$pages_arr = $onepager->content()->getPages();
-	$blocks    = array_values( (array) $onepager->blockManager()->all() );
+  $ajaxUrl = $onepager->api()->getAjaxUrl();
+  $nav_arr = $onepager->content()->getMenus();
+  $cat_arr = $onepager->content()->getCategories();
+  $pages_arr = $onepager->content()->getPages();
+  $blocks = array_values((array)$onepager->blockManager()->all());
   $groupOrder = $onepager->blockManager()->getGroupOrder();
 
-	$sections = array_map( function ( $section ) {
-		$section = onepager()->render()->sectionBlockDataMerge($section);
-		$section['content'] = onepager()->render()->section( $section );
-		$section['style']   = onepager()->render()->style( $section );
+  $sections = array_map(function ($section) {
+    $section = onepager()->render()->sectionBlockDataMerge($section);
+    $section['content'] = onepager()->render()->section($section);
+    $section['style'] = onepager()->render()->style($section);
 
-		return $section;
-	}, onepager()->section()->all( $pageId ) );
+    return $section;
+  }, onepager()->section()->all($pageId));
 
-	$disableUrl   = getOpBuildModeUrl( getCurrentPageURL(), false);
+  $disableUrl = getOpBuildModeUrl(getCurrentPageURL(), false);
 
-	return array(
-		'ajaxUrl'    	=> $ajaxUrl,
-		'optionPanel'   => onepager()->optionsPanel("onepager")->getOptions(),
-		'options'	    => get_option('onepager'),
-		'page'     		=> 'onepager',
-		'blocks'     => $blocks,
-		'pageId'     => $pageId,
-		'sections'   => $sections,
-		'menus'      => $nav_arr,
-		'pages'      => $pages_arr,
-		'categories' => $cat_arr,
-    'groupOrder' => $groupOrder,
-		'disable'		=> $disableUrl
-	);
+  return array(
+    'ajaxUrl'     => $ajaxUrl,
+    'optionPanel' => onepager()->optionsPanel("onepager")->getOptions(),
+    'options'     => get_option('onepager'),
+    'page'        => 'onepager',
+    'blocks'      => $blocks,
+    'pageId'      => $pageId,
+    'sections'    => $sections,
+    'menus'       => $nav_arr,
+    'pages'       => $pages_arr,
+    'categories'  => $cat_arr,
+    'groupOrder'  => $groupOrder,
+    'disable'     => $disableUrl,
+  );
 }
 
-function enqueueOnepagerAssets() {
-	$q = onepager()->asset();
+function enqueueOnepagerAssets()
+{
+  if(!onepager()->content()->isOnepage()) return;
 
-	// TX namespaced assets to avoid multiple assets loading from other ThemeXpert product
-	$q->style( 'tx-bootstrap', asset( 'assets/css/bootstrap.css' ) );
-	$q->style( 'tx-animatecss', asset( 'assets/css/animate.css' ) );
-	$q->style( 'tx-fontawesome', asset( 'assets/css/font-awesome.css' ) );
+  $q = onepager()->asset();
 
-	$q->script( 'tx-bootstrap', asset( 'assets/js/bootstrap.js' ), [ 'jquery' ] );
-	$q->script( 'tx-wow', asset('assets/js/wow.js'), array( 'jquery' ) );
-	$q->script( 'tx-nicescroll', asset('assets/js/jquery.nicescroll.js'), array( 'jquery' ) );
-	$q->script( 'lithium', asset('assets/lithium.js'), array( 'jquery' ) );
+  // TX namespaced assets to avoid multiple assets loading from other ThemeXpert product
+  $q->style('tx-bootstrap', asset('assets/css/bootstrap.css'));
+  $q->style('tx-animatecss', asset('assets/css/animate.css'));
+  $q->style('tx-fontawesome', asset('assets/css/font-awesome.css'));
 
-  $q->style( 'lithium', asset( 'assets/css/lithium.css' ) );
+  $q->script('tx-bootstrap', asset('assets/js/bootstrap.js'), ['jquery']);
+  $q->script('tx-wow', asset('assets/js/wow.js'), array('jquery'));
+  $q->script('tx-nicescroll', asset('assets/js/jquery.nicescroll.js'), array('jquery'));
+  $q->script('lithium', asset('assets/lithium.js'), array('jquery'));
 
-  if(is_super_admin()){
-    $q->style( 'lithium-ui', asset( 'assets/css/lithium-builder.css' ) );
+  $q->style('lithium', asset('assets/css/lithium.css'));
+
+  if (is_super_admin()) {
+    $q->style('lithium-ui', asset('assets/css/lithium-builder.css'));
   }
 
-	if ( onepager()->content()->isBuildMode() ) {
-		if ( function_exists( 'wp_enqueue_media' ) ) {
-			wp_enqueue_media();
-		}
+  if (onepager()->content()->isBuildMode()) {
+    if (function_exists('wp_enqueue_media')) {
+      wp_enqueue_media();
+    }
 
-		$q->script( 'tx-iconselector', asset( 'assets/js/icon-selector.min.js' ), [ 'jquery' ] );
-		$q->script( 'tx-colorpicker', asset( 'assets/js/bootstrap-colorpicker.js' ), [ 'jquery' ] );
-		$q->script( 'tx-bootstrap-switch', asset( 'assets/js/bootstrap-switch.js' ), [ 'jquery' ] );
-		$q->script( 'tx-toastr', asset( 'assets/js/toastr.js' ), [ 'jquery' ] );
+    $q->script('tx-iconselector', asset('assets/js/icon-selector.min.js'), ['jquery']);
+    $q->script('tx-colorpicker', asset('assets/js/bootstrap-colorpicker.js'), ['jquery']);
+    $q->script('tx-bootstrap-switch', asset('assets/js/bootstrap-switch.js'), ['jquery']);
+    $q->script('tx-toastr', asset('assets/js/toastr.js'), ['jquery']);
 
-		$q->script( 'onepager', asset('assets/app.bundle.js'), ['jquery']);
+    $q->script('onepager', asset('assets/app.bundle.js'), ['jquery']);
 
-		$q->localizeScript( 'onepager', getOnepagerData( onepager()->content()->getCurrentPageId() ), 'onepager' );
-	}
+    $q->localizeScript('onepager', getOnepagerData(onepager()->content()->getCurrentPageId()), 'onepager');
+  }
 
 }
 
-function enqueueOnepagerAdminAssets(){
-	$q = onepager()->asset();
+function enqueueOnepagerAdminAssets()
+{
+  $q = onepager()->asset();
 
-	if ( function_exists( 'wp_enqueue_media' ) ) {
-		wp_enqueue_media();
-	}
+  if (function_exists('wp_enqueue_media')) {
+    wp_enqueue_media();
+  }
 
-	$q->style( 'tx-bootstrap', asset( 'assets/css/bootstrap.css' ) );
-	$q->style( 'tx-animatecss', asset( 'assets/css/animate.css' ) );
-	$q->style( 'tx-fontawesome', asset( 'assets/css/font-awesome.css' ) );
-	$q->style( 'lithium-ui', asset( 'assets/css/lithium-builder.css' ) );
+  $q->style('tx-bootstrap', asset('assets/css/bootstrap.css'));
+  $q->style('tx-animatecss', asset('assets/css/animate.css'));
+  $q->style('tx-fontawesome', asset('assets/css/font-awesome.css'));
+  $q->style('lithium-ui', asset('assets/css/lithium-builder.css'));
 
-	$q->script( 'tx-bootstrap', asset( 'assets/js/bootstrap.js' ), [ 'jquery' ] );
+  $q->script('tx-bootstrap', asset('assets/js/bootstrap.js'), ['jquery']);
 
-	$q->script( 'tx-iconselector', asset( 'assets/js/icon-selector.min.js' ), [ 'jquery' ] );
-	$q->script( 'tx-colorpicker', asset( 'assets/js/bootstrap-colorpicker.js' ), [ 'jquery' ] );
-	$q->script( 'tx-toastr', asset( 'assets/js/toastr.js' ), [ 'jquery' ] );
+  $q->script('tx-iconselector', asset('assets/js/icon-selector.min.js'), ['jquery']);
+  $q->script('tx-colorpicker', asset('assets/js/bootstrap-colorpicker.js'), ['jquery']);
+  $q->script('tx-toastr', asset('assets/js/toastr.js'), ['jquery']);
 
-	$q->script( 'admin-bundle', asset('assets/optionspanel.bundle.js'), ['jquery']);
+  $q->script('admin-bundle', asset('assets/optionspanel.bundle.js'), ['jquery']);
 
   $q->enqueue();
 }
 
+function remove_theme_styles()
+{
+  if(!onepager()->content()->isOnepage()) return;
+
+  remove_action('wp_enqueue_scripts', 'twentythirteen_scripts_styles');
+  remove_action('wp_enqueue_scripts', 'twentyfourteen_scripts');
+  remove_action('wp_enqueue_scripts', 'twentyfifteen_scripts');
+}
+
+add_action('after_setup_theme', 'remove_theme_styles');
 //live edit mode
-add_action( 'wp_enqueue_scripts', 'enqueueOnepagerAssets' );
+add_action('wp_enqueue_scripts', 'enqueueOnepagerAssets');
