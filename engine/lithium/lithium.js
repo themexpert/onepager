8fae6ba506266c7f8e729f2c31cb5a697d2e491e
Nadim Tuhin <nadimtuhin@gmail.com>
2015-08-09 07:08:55 +0600

    [bugfix] only target navigation menus
    update all bocks complying to it

--- engine/lithium/lithium.js
+++ engine/lithium/lithium.js
@@ -1,6 +1,7 @@
 // Smooth scrolling
 jQuery(function() {
-  jQuery('a[href*=#]:not([href=#])').click(function() {
+  //only target navigation menus
+  jQuery('nav a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = jQuery(this.hash);
       var navbar = jQuery('.navbar').height() + 10; // minus navbar height
