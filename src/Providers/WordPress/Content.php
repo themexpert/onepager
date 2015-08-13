eb76679b717257aa0d193cac3a4b384354bda19e
Nadim Tuhin <nadimtuhin@gmail.com>
2015-08-13 18:26:39 +0600

    [bugfix] select was comming bottom

--- src/Providers/WordPress/Content.php
+++ src/Providers/WordPress/Content.php
@@ -4,85 +4,77 @@ use ThemeXpert\Providers\Contracts\ContentInterface;
 
 /**
  * Class Content
+ *
  * @package ThemeXpert\Providers\WordPress
  */
 class Content implements ContentInterface {
-	/**
-	 * @return array
-	 */
-	public function getPages() {
-		return $this->objAsArray( get_pages(), 'ID', 'post_title' )+[""=>"Select"];
-	}
-
-	/**
-	 * @param $obj
-	 * @param $oKey
-	 * @param $oValue
-	 *
-	 * @return array
-	 */
-	protected function objAsArray( $obj, $oKey, $oValue ) {
-		$arr = [ ];
-
-		array_walk( $obj, function ( $v, $k ) use ( &$arr, $oKey, $oValue ) {
-			$arr[ $v->{$oKey} ] = $v->{$oValue};
-		} );
-
-		return $arr;
-	}
-
-	/**
-	 *
-	 */
-	public function getPosts() {
-		// TODO: Implement getPosts() method.
-	}
-
-	/**
-	 * @return array
-	 */
-	public function getMenus() {
-		return $this->objAsArray( get_terms( 'nav_menu', array('hide_empty'=> 0) ), 'term_id', 'name' ) + [""=>"Select"];
-	}
-
-	/**
-	 * @return array
-	 */
-	public function getCategories() {
-		return [""=>"select"]+$this->objAsArray( get_terms( 'category', array('hide_empty'=> 0) ), 'term_id', 'name' );
-	}
-
-	/**
-	 *
-	 */
-	public function getMenuLocations() {
-		// TODO: Implement getMenuLocations() method.
-	}
-
-  public function isBuildMode() {
-      return is_super_admin() && $this->isOnepage() && (array_key_exists('onepager', $_GET) ? (int) $_GET['onepager'] : 0);
+  /**
+   * @return array
+   */
+  public function getPages() {
+    return ["Select"] + obj_to_array(get_pages(), 'ID', 'post_title');
   }
 
-	public function isOnepagerByTemplate(){
-		$template = get_post_meta( $this->getCurrentPageId(), '_wp_page_template', true );
+  /**
+   *
+   */
+  public function getPosts() {
+    // TODO: Implement getPosts() method.
+  }
 
-		//template name is onepage.php or op-*.php
-    return ( $template == "onepage.php" || substr($template, 0, 3) == "op-" ) ? true : false;
-	}
+  /**
+   * @return array
+   */
+  public function getMenus() {
+    return ["Select"] + obj_to_array(get_terms('nav_menu', array('hide_empty' => 0)), 'term_id', 'name');
+  }
 
-	public function isOnepagerByMeta() {
-		$onepager = get_post_meta( $this->getCurrentPageId(), '_onepager_updated', true );
+  /**
+   * @return array
+   */
+  public function getCategories() {
+    return ["select"] + obj_to_array(get_terms('category', array('hide_empty' => 0)), 'term_id', 'name');
+  }
 
-		return $onepager ? true : false;
-	}
+  /**
+   *
+   */
+  public function getMenuLocations() {
+    // TODO: Implement getMenuLocations() method.
+  }
 
-	public function isOnepage() {
-		return $this->isOnepagerByTemplate() || $this->isOnepagerByMeta();
-	}
+  public function isBuildMode() {
+    return is_super_admin() && $this->isOnepage() && (array_key_exists('onepager', $_GET) ? (int)$_GET['onepager'] : 0);
+  }
 
-	public function getCurrentPageId() {
-		global $post;
+  public function isOnepagerByTemplate($pageId = null) {
+    if (!$pageId) {
+      $pageId = $this->getCurrentPageId();
+    }
 
-		return $post && $post->ID ? $post->ID : null;
-	}
+    $template = get_post_meta($pageId, '_wp_page_template', true);
+
+    //template name is onepage.php or op-*.php
+    return ($template == "onepage.php" || substr($template, 0, 3) == "op-") ? true : false;
+  }
+
+  public function isOnepagerByMeta() {
+    $onepager = get_post_meta($this->getCurrentPageId(), '_onepager_updated', true);
+
+    return $onepager ? true : false;
+  }
+
+  public function isOnepage($pageId = null) {
+    if (!$pageId) {
+      $pageId = $this->getCurrentPageId();
+    }
+
+    return $this->isOnepagerByTemplate($pageId) || $this->isOnepagerByMeta();
+  }
+
+  public function getCurrentPageId() {
+    global $post;
+
+    return $post && $post->ID ? $post->ID : null;
+  }
 }
