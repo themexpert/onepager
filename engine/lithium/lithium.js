567c3f7cb37bdc8e9f4e5f53f6522a5a207cc398
Nadim Tuhin <nadimtuhin@gmail.com>
2015-08-14 23:30:15 +0600

    Nav menu smooth scrolling fixed fix #9

--- engine/lithium/lithium.js
+++ engine/lithium/lithium.js
@@ -1,11 +1,10 @@
 // Smooth scrolling
 jQuery(function() {
   //only target navigation menus
-  jQuery('nav a[href*=#]:not([href=#])').click(function() {
+  jQuery('nav a[href*=#]:not([href=#]),.nav a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = jQuery(this.hash);
       var navbar = jQuery('.navbar').height() + 10; // minus navbar height
-      console.log(navbar);
       target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         jQuery('html,body').animate({
