856536d0b54271010c4b8d57ab12d9a013166c5f
Nadim Tuhin <nadimtuhin@gmail.com>
2015-08-14 12:19:51 +0600

    [bugfix] Footer menu dropdown fixed fix #7

--- blocks/footers/footer-1/view.php
+++ blocks/footers/footer-1/view.php
@@ -1,15 +1,16 @@
 <footer id="<?php echo $id; ?>" class="op-section text-center footers footer-1">
-	<div class="social-links">
-	<?php foreach($contents['social'] as $social):?>
-		<a class="icon" href="<?php echo $social?>" target="_blank"></a>
-	<?php endforeach;?>
-	</div>
-	<!-- WP Nav -->
-	<?php wp_nav_menu(array(
-        'menu' =>$contents['menu'] ,
-        'menu_class'=>'nav nav-pills flex flex-center',
-        'container' =>false,
-    )) ?>
+  <div class="social-links">
+    <?php foreach ( $contents['social'] as $social ): ?>
+      <a class="icon" href="<?php echo $social ?>" target="_blank"></a>
+    <?php endforeach; ?>
+  </div>
+  <!-- WP Nav -->
+  <?php wp_nav_menu( array(
+    'menu'       => $contents['menu'],
+    'menu_class' => 'nav nav-pills flex flex-center',
+    'container'  => false,
+    'walker'     => new wp_bootstrap_navwalker(),
+  ) ) ?>
 
-    <p class="copyright"><?php echo $contents['copyright']?></p>
+  <p class="copyright"><?php echo $contents['copyright'] ?></p>
 </footer>
