dcbb926db1b0a8cecff133476df4875c0ab99779
Parvez Akther <parvez@themexpert.com>
2015-08-21 16:17:44 +0600

    Documentation link added to the global fotter and link wrapper updated

--- src/theme_helpers.php
+++ src/theme_helpers.php
@@ -5,7 +5,7 @@ if( !function_exists('op_link') )
 {
   function op_link($link, $class)
   {
-    if( $link['url'] OR $link['text'] ){
+    if( $link['url'] AND $link['text'] ){
       $target = ($link['target']) ? '_blank' : '_self';
       return '<a class="'.$class.'" href="'. $link['url'] .'" target="'.$target.'">'. $link['text'] .'</a>';
     }
