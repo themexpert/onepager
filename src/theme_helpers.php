378e17d3071224731379bdd828c9f313af8f7015
Nadim Tuhin <nadimtuhin@gmail.com>
2015-09-15 20:22:44 +0600

    enhanced theme helpers link function to add default # link if no url specified

--- src/theme_helpers.php
+++ src/theme_helpers.php
@@ -3,12 +3,14 @@
 // Wrapper function for link
 if ( ! function_exists( 'op_link' ) ) {
   function op_link( $link, $class = '' ) {
-    if ( $link['url'] AND $link['text'] ) {
+    $link['url'] = $link['url']?:"#";
+
+    if ( $link['text'] ) {
       $target = ( $link['target'] ) ? '_blank' : '_self';
 
       return '<a class="' . $class . '" href="' . $link['url'] . '" target="' . $target . '">' . $link['text'] . '</a>';
     }
 
-    return;
+    return '';
   }
 }
