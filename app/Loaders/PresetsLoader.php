0ee8798a472487917063c49b0157da3c54ec6037
Nadim Tuhin <nadimtuhin@gmail.com>
2015-10-02 16:33:50 +0600

    deprecated two methods in Onepager and loading presets from theme automatically

--- app/Loaders/PresetsLoader.php
+++ app/Loaders/PresetsLoader.php
@@ -8,6 +8,7 @@ class PresetsLoader {
     $this->presetManager = $presetManager;
 
     add_action( 'admin_init', [ $this, 'onepagerPresetsLoader' ] );
+    add_action( 'admin_init', [ $this, 'themePresetsLoader' ] );
   }
 
   public function onepagerPresetsLoader() {
@@ -15,5 +16,26 @@ class PresetsLoader {
 
     $this->presetManager->loadAllFromPath(
       ONEPAGER_PRESETS_PATH, ONEPAGER_PRESETS_URL, $groups );
+  }
+
+  public function themePresetsLoader() {
+    $groups = [$this->getCurrentThemeBlocksGroup()];
+
+    $dir = ONEPAGER_THEME_PATH . "/presets";
+    $url = ONEPAGER_THEME_URL . "/presets";
+
+    if ( ! file_exists( $dir ) ) {
+      return;
+    }
+
+    $this->presetManager->loadAllFromPath( $dir, $url, $groups );
+  }
+
+  /**
+   * @return string
+   */
+  private function getCurrentThemeBlocksGroup() {
+    $currentTheme = wp_get_theme();
+    return $currentTheme->get( 'Name' ) . " (theme)";
   }
 }
--- app/Onepager.php
+++ app/Onepager.php
@@ -1,90 +1,84 @@
 <?php
+
 /**
  * Class to proive simple API. No need to call chain method or complex query.
  */
-
 abstract class Onepager {
 
-	// Get the global option panel object
-  protected static $presets = [];
+  // Get the global option panel object
+  protected static $presets = [ ];
   protected static $basePreset;
 
-  public static function getOptionPanel()
-	{
-		return onepager()->optionsPanel("onepager");
-	}
-	// Get individual option
-	public static function getOption($name, $default="")
-	{
-		return onepager()->optionsPanel('onepager')->getOption($name, $default);
-	}
-	// Register Custom Blocks Folder
-	public static function registerBlocks($folder = 'blocks')
-	{
-		onepager()->blockManager()->loadAllFromPath(
-		  get_template_directory() . '/' . $folder,
-		  get_template_directory_uri() . '/' . $folder
-		);
-	}
+  public static function getOptionPanel() {
+    return onepager()->optionsPanel( "onepager" );
+  }
 
-	// Disable Any Blocks Group
+  // Get individual option
+  public static function getOption( $name, $default = "" ) {
+    return onepager()->optionsPanel( 'onepager' )->getOption( $name, $default );
+  }
+
+  /**
+   * Deprecated from 1.1.4
+   * Will be removed in 1.2
+   * @param string $folder
+   */
+  public static function registerBlocks( $folder = 'blocks' ) {
+
+  }
+
+  // Disable Any Blocks Group
   public static function disableBlocks( $groups ) {
-    onepager()->blockManager()->setIgnoredGroups($groups);
+    onepager()->blockManager()->setIgnoredGroups( $groups );
   }
 
-	// Disable all core blocks group = onepager
-	public static function disableCoreBlocks()
-	{
-		self::disableBlocks('onepager');
-	}
-
-	// Register Preset Paths
-	public static function registerPresets( $folder = 'presets')
-	{
-		onepager()->presetManager()->loadAllFromPath(
-			get_template_directory() . '/' . $folder,
-			get_template_directory_uri() . '/' . $folder,
-			"onepager"
-		);
-	}
-
-	// Disable Any Presets Group
-  public static function disablePresets($groups)
-	{
-    onepager()->presetManager()->setIgnoredGroups($groups);
+  // Disable all core blocks group = onepager
+  public static function disableCoreBlocks() {
+    self::disableBlocks( 'onepager' );
   }
 
-	// Disable core presets
-	public static function disableCorePresets()
-	{
-		self::disablePresets('onepager');
-	}
+  /**
+   * Deprecated from 1.1.4
+   * Will be removed in 1.2
+   * @param string $folder
+   */
+  public static function registerPresets( $folder = 'presets' ) {
 
-  public static function addPresets($name, $presets){
-    if(!array_key_exists($name, static::$presets)){
-      static::$presets[$name] = [];
+  }
+
+  // Disable Any Presets Group
+  public static function disablePresets( $groups ) {
+    onepager()->presetManager()->setIgnoredGroups( $groups );
+  }
+
+  // Disable core presets
+  public static function disableCorePresets() {
+    self::disablePresets( 'onepager' );
+  }
+
+  public static function addPresets( $name, $presets ) {
+    if ( ! array_key_exists( $name, static::$presets ) ) {
+      static::$presets[ $name ] = [ ];
     }
 
-    static::$presets[$name] = array_merge($presets, static::$presets[$name]);
+    static::$presets[ $name ] = array_merge( $presets, static::$presets[ $name ] );
   }
 
   public static function getPresets() {
     return static::$presets;
   }
 
-	// Add style
-	public static function addStyle($name, $src = false, $dependency = [], $version = ONEPAGER_VERSION, $media = 'all')
-	{
-		onepager()->asset()->style( $name, $src, $dependency, $version, $media );
-	}
+  // Add style
+  public static function addStyle( $name, $src = false, $dependency = [ ], $version = ONEPAGER_VERSION, $media = 'all' ) {
+    onepager()->asset()->style( $name, $src, $dependency, $version, $media );
+  }
 
-	// Add script
-	public static function addScript($name, $src = false, $dependency = [], $version = ONEPAGER_VERSION, $footer = true)
-	{
-			onepager()->asset()->script($name, $src, $dependency, $version, $footer);
-	}
+  // Add script
+  public static function addScript( $name, $src = false, $dependency = [ ], $version = ONEPAGER_VERSION, $footer = true ) {
+    onepager()->asset()->script( $name, $src, $dependency, $version, $footer );
+  }
 
-  public static function basePreset($array) {
+  public static function basePreset( $array ) {
     static::$basePreset = $array;
   }
 
