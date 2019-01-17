015ab5387fb9d69d40ab53244c21280bc5ef06e8
Parvez Akther <parvez@themexpert.com>
2019-01-17 14:49:42 +0600

    [Block Refactored] Navbar 1 done

--- blocks/navbars/navbar-1/view.php
+++ blocks/navbars/navbar-1/view.php
@@ -1,33 +1,45 @@
-<header id="<?php echo $id; ?>" class="op-section navbar navbar-static-top navbar-1" <?php echo ($settings['sticky_nav']) ? 'data-spy="affix"' : '';?> data-offset-top="80">
-	<div class="container">
-		<!-- Brand -->
-	    <div class="navbar-header">
-	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-<?php echo $id; ?>">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
-	      <a class="navbar-brand" href="<?php echo site_url(); ?>">
-	      	<?php if($contents['logo']) :?>
-				<img class="img-responsive" src="<?php echo $contents['logo']?>" alt="<?php wp_title(); ?>">
-			<?php else : ?>
-				<?php wp_title(); ?>
-			<?php endif; ?>
-	      </a>
-	    </div>
-	    <!-- Menu -->
-	    <nav class="collapse navbar-collapse" id="nav-<?php echo $id; ?>">
-				<!-- Link -->
-				<?php echo op_link($contents['link'], 'btn navbar-btn navbar-right');?>
+<?php
+	$sticky = ($settings['sticky_nav']) ? 'uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky"' : '';
+?>
+<div id="<?php echo $id; ?>" class="uk-container navbar-1" <?php echo $sticky; ?>>
+	<nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+			<div class="uk-navbar-left">
+					<?php if($contents['logo']) :?>
+						<a class="uk-navbar-item uk-logo" href="#"><img class="img-responsive" src="<?php echo $contents['logo']?>" alt="<?php wp_title(); ?>"/></a>
+					<?php else : ?>
+					<a class="uk-navbar-item uk-logo" href="<?php echo site_url(); ?>"><?php wp_title(); ?></a>
+					<?php endif; ?>
+			</div>
+			<div class="uk-navbar-right">
+				<?php wp_nav_menu(array(
+					'menu' =>$contents['menu'] ,
+					'menu_class'=>'uk-navbar-nav uk-visible@m',
+					'container' =>false,
+					'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+					'walker'            => new wp_bootstrap_navwalker()
+				)) ?>
+				<?php echo op_link($contents['link'], 'uk-button uk-button-primary');?>
 				
-	    	<?php wp_nav_menu(array(
-                'menu' =>$contents['menu'] ,
-                'menu_class'=>'nav navbar-nav navbar-right',
-                'container' =>false,
-                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                'walker'            => new wp_bootstrap_navwalker()
-            )) ?>
-	    </nav>
+				<a uk-navbar-toggle-icon="" href="#offcanvas-<?php echo $id; ?>" uk-toggle class="uk-navbar-toggle uk-hidden@m uk-navbar-toggle-icon uk-icon">
+					<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="navbar-toggle-icon"><rect y="9" width="20" height="2"></rect><rect y="3" width="20" height="2"></rect><rect y="15" width="20" height="2"></rect></svg>
+				</a>
+			</div>
+	</nav>
+</div>
+
+<!-- This is the off-canvas -->
+<div id="offcanvas-<?php echo $id; ?>" uk-offcanvas="flip: true; overlay: true">
+	<div class="uk-offcanvas-bar">
+
+		<button class="uk-offcanvas-close" type="button" uk-close></button>
+
+		<?php wp_nav_menu(array(
+				'menu' =>$contents['menu'] ,
+				'menu_class'=>'uk-nav uk-nav-default',
+				'container' =>false,
+				'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+				'walker'            => new wp_bootstrap_navwalker()
+			)) ?>
+
 	</div>
-</header>
+</div>
