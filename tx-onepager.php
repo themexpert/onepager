9fbc377a8dd6f090d9aeba1944c1c4a1bd11ceb6
Parvez Akther <parvez@themexpert.com>
2019-01-19 13:00:46 +0600

    version num bumped 2.0

--- tx-onepager.php
+++ tx-onepager.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:       Onepager - One Page Builder
- * Plugin URI:        http://getonepager.com
+ * Plugin Name:       OnePager
+ * Plugin URI:        https://themesgrove.com/onepager
  * Description:       Modern, Powerful & Crazy Fast one page builder. Built with modern tools such ReactJS for next generation theming.
- * Version:           1.2.8.1
- * Author:            ThemeXpert
- * Author URI:        http://www.themexpert.com
+ * Version:           2.0.0
+ * Author:            ThemeXpert, Themesgrove
+ * Author URI:        https://themesgrove.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       tx-onepager
@@ -18,7 +18,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 if(!defined('ONEPAGER_VERSION')){
-  define( 'ONEPAGER_VERSION', '1.2.8.1' );
+  define( 'ONEPAGER_VERSION', '2.0.0' );
 }
 
 if(!defined('ONEPAGER_PHP_VERSION')) {
