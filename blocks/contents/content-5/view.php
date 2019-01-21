b5d935db36f9ac2201f543258a0a786d984961c1
Abdullah Al Mamun <mamunhai@gmail.com>
2019-01-21 19:21:35 +0600

    Changed Contet 5 Values

--- blocks/contents/content-5/view.php
+++ blocks/contents/content-5/view.php
@@ -1,27 +1,34 @@
 <?php
-	$animation = ($settings['animation']) ? $settings['animation'] : '';
+	// Content Animation
+	$content_animation = ($settings['content_animation']) ? 'uk-scrollspy="cls:uk-animation-'.$settings['content_animation'].'"' : '';
+	// Content Alignment
+	$content_alignment = ($settings['content_alignment']) ? $settings['content_alignment'] : '';
 ?>
-<section id="<?php echo $id;?>" class="op-section contents content-5 full-screen">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<div class="ml-big mr-big text-center">
+<section id="<?php echo $id;?>" class="uk-section contents content-5" <?php echo ($styles['bg_parallax']) ? 'uk-parallax="bgy: -200' : '' ?>>
+
+	<div class="uk-container">
+		<div class="uk-grid-large" <?php echo $content_animation;?> uk-grid>
+			<div class="uk-width-1-1">
+				<div class="uk-text-<?php echo $content_alignment;?>">
 					<!-- Image -->
 					<?php if($contents['image']) :?>
-						<img class="op-media wow <?php echo $animation;?>" src="<?php echo $contents['image']?>" alt="<?php echo $contents['title']?>">
+						<img class="op-media" src="<?php echo $contents['image'];?>" alt="<?php echo $contents['title'];?>">
 					<?php endif; ?>
+
 					<!-- Title -->
 					<?php if($contents['title']): ?>
-						<h1 class="section-title <?php echo $settings['title_transformation']?> <?php echo $settings['title_size']?> wow <?php echo $animation;?>"><?php echo $contents['title']?></h1>
+						<h1 class="uk-heading-primary uk-text-<?php echo $settings['title_transformation'];?> <?php echo $settings['title_size'];?>"><?php echo $contents['title'];?></h1>
 					<?php endif; ?>
+
 					<!-- Description -->
 					<?php if($contents['description']): ?>
-						<div class="section-desc wow <?php echo $animation;?>"><?php echo $contents['description']?></div>
+						<div class="uk-text-lead"><?php echo $contents['description'];?></div>
 					<?php endif; ?>
 					<!-- Link -->
-					<?php echo op_link($contents['link'], 'btn btn-primary btn-lg wow' . $animation);?>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+					<?php echo op_link($contents['link'], 'uk-button uk-button-primary uk-button-large');?>
+				</div> <!-- text-alignment -->
+
+			</div> <!-- uk-grid-1 -->
+		</div><!-- uk-grid-large -->
+	</div><!-- uk-container -->
+</section> <!-- end-section -->
