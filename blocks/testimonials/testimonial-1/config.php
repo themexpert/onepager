415265151f44ed71e3769a0a473eb6d3cbae515f
Abdullah Al Mamun <mamunhai@gmail.com>
2019-01-22 18:47:30 +0600

    Changed Testimonial 1 all of the vaule now working fine.

--- blocks/testimonials/testimonial-1/config.php
+++ blocks/testimonials/testimonial-1/config.php
@@ -8,93 +8,138 @@ return array(
   // Fields - $contents available on view file to access the option
   'contents' => array(
     array(
-      'name' => 'title',
-      'value' => 'People Love Us'
-    ),
-    array(
       'name'=>'testimonials',
       'type'=>'repeater',
       'fields' => array(
         array(
-          array('name'=>'name', 'value' => 'John Resig'),
-          array('name'=>'designation', 'value' => 'Creator of jQuery'),
           array('name'=>'image', 'label' => 'Image', 'type'=>'image', 'value' => 'http://s3.amazonaws.com/quantum-assets/images/8-thumb.jpg' ),
           array('name'=>'testimony', 'type'=> 'textarea', 'value' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis ex risus. Vivamus hendrerit nec ex vitae varius. Aliquam sollicitudin dapibus dapibus. Duis lacus diam, lacinia a fringilla semper, laoreet eget tellus. Vestibulum sed nisi rutrum, efficitur odio et, varius mi.'),
+          array('name'=>'name', 'value' => 'John Resig'),
+          array('name'=>'designation', 'value' => 'Creator of jQuery'),
         ),
         array(
-          array('name'=>'name', 'value' => 'Elon Musk'),
-          array('name'=>'designation', 'value' => 'CEO and CTO of SpaceX'),
           array('name'=>'image','type'=>'image', 'value' => 'http://s3.amazonaws.com/quantum-assets/images/7-thumb.jpg'),
           array('name'=>'testimony', 'type'=> 'textarea', 'value' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis ex risus. Vivamus hendrerit nec ex vitae varius. Aliquam sollicitudin dapibus dapibus. Duis lacus diam, lacinia a fringilla semper, laoreet eget tellus. Vestibulum sed nisi rutrum, efficitur odio et, varius mi.'),
+          array('name'=>'name', 'value' => 'Elon Musk'),
+          array('name'=>'designation', 'value' => 'CEO and CTO of SpaceX'),
         )
-      )
-    )
-
-  ),
-
-  // Settings - $settings available on view file to access the option
-  'settings' => array(
-    array(
-      'name' => 'title_size',
-      'label' => 'Title Size',
-      'append' => 'px',
-      'value' => '@section_title_size'
-    ),
-    array(
-      'name' => 'interval',
-      'label' => 'Slider Interval',
-      'value' => 3
-    ),
-    array(
-      'name'     => 'title_transformation',
-      'label'    => 'Title Transformation',
-      'type'     => 'select',
-      'value'    => 'text-uppercase',
-      'options'  => array(
-        'text-lowercase'   => 'Lowercase',
-        'text-uppercase'   => 'Uppercase',
-        'text-capitalize'  => 'Capitalized'
       )
     )
   ),
 
   'styles' => array(
+
     array(
       'name'  => 'bg_image',
       'label' => 'Background Image',
       'type'  => 'image'
     ),
-    array(
-      'name'     => 'bg_repeat',
-      'label'    => 'Image Repeat',
-      'type'     => 'select',
-      'value'    => 'no-repeat',
-      'options'  => array(
-        'no-repeat'     => 'No Repeat',
-        'repeat-x'      => 'Repeat X',
-        'repeat-y'      => 'Repeat Y',
-      )
-    ),
-    array(
-      'name'    => 'bg_color',
-      'label'   => 'Background Color',
-      'type'    => 'colorpicker',
-      'value'   => '#f5f5f5'
-    ),
+    // array(
+    //   'name'     => 'bg_repeat',
+    //   'label'    => 'Image Repeat',
+    //   'type'     => 'select',
+    //   'value'    => 'no-repeat',
+    //   'options'  => array(
+    //     'no-repeat'     => 'No Repeat',
+    //     'repeat-x'      => 'Repeat X',
+    //     'repeat-y'      => 'Repeat Y',
+    //   )
+    // ),
 
     array(
       'name'=>'bg_parallax',
       'type'=> 'switch',
       'label'=>'Parallax Background'
     ),
+
     array(
-      'name'    => 'text_color',
-      'label'   => 'Color',
+      'name'    => 'overlay_color',
+      'label'   => 'Overlay Color',
       'type'    => 'colorpicker',
+    ),
+
+    array(
+      'name'    => 'testimoni_color',
+      'label'   => 'Testimoni Color',
+      'type'    => 'colorpicker',
+      'value' => '#fff'
+    ),
+    array(
+      'name'    => 'name_color',
+      'label'   => 'Name Color',
+      'type'    => 'colorpicker',
+      'value' => '#fff'
+    ),
+    array(
+      'name'    => 'designation_color',
+      'label'   => 'Designation Color',
+      'type'    => 'colorpicker',
+      'value' => '#fff'
+    ),
+
+    array(
+      'name'    => 'dot_color',
+      'label'   => 'Dot Color',
+      'type'    => 'colorpicker',
+      'value' => '#fff'
     ),
   ),
 
+  // Settings - $settings available on view file to access the option
+  'settings' => array(
+    array(
+      'name'     => 'animation',
+      'label'    => 'Animation',
+      'type'     => 'select',
+      'value'    => 'slide',
+      'options'  => array(
+        'slide'   => 'Slide',
+        'fade'   => 'Fade',
+        'scale'  => 'Scale',
+        'pull'  => 'Pull',
+        'push'  => 'Push'
+      ),
+    ),
+    array(
+      'name' => 'autoplay',
+      'label' => 'Autoplay',
+      'type' => 'switch',
+      'value' => true
+    ),
+    array(
+      'name' => 'slider_height',
+      'label' => 'Slider Height',
+      'append' => 'px',
+    ),
+    array(
+      'name' => 'name_size',
+      'label' => 'Name Size',
+      'append' => 'px',
+      'value' => '32'
+    ),
+    array(
+      'name'     => 'name_transformation',
+      'label'    => ' Transformation',
+      'type'     => 'select',
+      'value'    => 0,
+      'options'  => array(
+        0 => 'Default',
+        'lowercase'   => 'Lowercase',
+        'uppercase'   => 'Uppercase',
+        'capitalize'  => 'Capitalized'
+      ),
+
+    ),
+      array(
+        'name' => 'designation_size',
+        'label' => 'Designation Size',
+        'append' => 'px',
+        'value' => '20'
+      ),
+    
+  ),
+
   'assets' => function( $path ){
-    onepager()->asset()->style( 'testimonial-1', $path . '/style.css' );
+    Onepager::addStyle('testimonial-1', $path . '/style.css');
   }
 );
