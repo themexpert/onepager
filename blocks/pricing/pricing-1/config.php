d3f995afb848a83749c04361b2b72ba69c9e4ed6
Abdullah Al Mamun <mamunhai@gmail.com>
2019-01-23 17:39:19 +0600

    Changed Pricing 1 all of the values and added the style.css

--- blocks/pricing/pricing-1/config.php
+++ blocks/pricing/pricing-1/config.php
@@ -8,7 +8,7 @@ return array(
     array('name'=>'title', 'value'=>'Price'),
     array('name'=>'description', 'value'=>'Our pricing is awesome', 'type'=>'editor'),
     array(
-      'name'=>'packages',
+      'name'=>'pricings',
       'type'=>'repeater',
       'fields'=> array(
         array(
@@ -40,5 +40,138 @@ return array(
         ),
       )
     )
-  )
+  ),
+
+
+    // Settings - $settings available on view file to access the option
+  'settings' => array(
+    array('label'=>'Heading', 'type'=>'divider'), // Divider - Text
+    array(
+      'name' => 'title_size',
+      'label' => 'Title Size',
+      'append' => 'px',
+      'value' => '@section_title_size'
+    ),
+    array(
+      'name'     => 'title_transformation',
+      'label'    => 'Title Transformation',
+      'type'     => 'select',
+      'value'    => 'inherit',
+      'options'  => array(
+        'inherit' => 'Default',
+        'lowercase'   => 'Lowercase',
+        'uppercase'   => 'Uppercase',
+        'capitalize'  => 'Capitalized'
+      )
+    ),
+
+    array(
+      'name'     => 'title_alignment',
+      'label'    => 'Title Alignment',
+      'type'     => 'select',
+      'value'    => 'center',
+      'options'  => array(
+        'left'      => 'Left',
+        'center'    => 'Center',
+        'right'     => 'Right',
+        'justify'   => 'Justify',
+      )
+    ),
+
+    array(
+      'name' => 'desc_size',
+      'label' => 'Desc Size',
+      'append' => 'px',
+      'value' => '18'
+    ),
+
+    array(
+      'name'     => 'title_animation',
+      'label'    => 'Animation',
+      'type'     => 'select',
+      'value'    => '0',
+      'options'  => array(        
+        '0'                     =>  'None',
+        'fade'                  =>  'Fade',
+        'scale-up'              =>  'Scale Up',
+        'scale-down'            =>  'Scale Down',
+        'slide-top-small'       =>  'Slide Top Small',
+        'slide-bottom-small'    =>  'Slide Bottom Small',
+        'slide-left-small'      =>  'Slide Left Small',
+        'slide-right-small'     =>  'Slide Right Small',
+        'slide-top-medium'      =>  'Slide Top Medium',
+        'slide-bottom-medium'   =>  'Slide Bottom Medium',
+        'slide-left-medium'     =>  'Slide Left Medium',
+        'slide-right-medium'    =>  'Slide Right Medium',
+        'slide-top'             =>  'Slide Top 100%',
+        'slide-bottom'          =>  'Slide Bottom 100%',
+        'slide-left'            =>  'Slide Left 100%',
+        'slide-right'           =>  'Slide Right 100%'
+
+      ),
+    ),
+
+    array('label'=>'Items', 'type'=>'divider'), // Divider - Text
+
+    array(
+      'name'     => 'items_columns',
+      'label'    => 'Columns',
+      'type'     => 'select',
+      'value'    => '3',
+      'options'  => array(
+        '2'   => '2',
+        '3'   => '3',
+        '4'   => '4',
+
+      ),
+    ),
+
+  ),
+
+  'styles' => array(
+    array(
+      'name'  =>'bg_color',
+      'label' => 'Background Color',
+      'type'  => 'colorpicker',
+      'value' => '#fff'
+    ),
+
+    array('label'=>'Heading', 'type'=>'divider'), // Divider - Text
+    array(
+      'name'=>'title_color',
+      'label' => 'Title Color',
+      'type'  => 'colorpicker',
+      'value' => '#323232'
+    ),
+
+    array(
+      'name'=>'desc_color',
+      'label' => 'Desc Color',
+      'type'  => 'colorpicker',
+      'value' => '#323232'
+    ),
+
+    array('label'=>'Items', 'type'=>'divider'), // Divider - Text
+
+
+    array(
+      'name'=>'feature_text_color',
+      'label' => 'Featured Text Color',
+      'type'  => 'colorpicker',
+      'value' => '#fff'
+    ),
+
+
+    array(
+      'name'=>'feature_bg_color',
+      'label' => 'Featured Bg Color',
+      'type'  => 'colorpicker',
+      'value' => '@color.primary'
+    ),
+  ),
+
+    'assets' => function( $path ){
+    // Local file
+    Onepager::addStyle('pricing-1', $path . '/style.css');
+  }
 );
