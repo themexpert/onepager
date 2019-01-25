68542e33b9e16e0cb7e531249dce7f2358736392
Abdullah Al Mamun <mamunhai@gmail.com>
2019-01-25 17:26:21 +0600

    pricing div dtructure done

--- blocks/pricing/pricing-1/view.php
+++ blocks/pricing/pricing-1/view.php
@@ -8,45 +8,40 @@
  ?>
 
 <section id="<?php echo $id;?>" class="uk-section pricing pricing-1">
-     <div class="uk-container">
-       
+    <div class="uk-container">
+        <div class="section-heading uk-text-<?php echo $title_alignment;?>" <?php echo $title_animation;?>>
+            <?php if($contents['title']):?>
+              <!-- Section Title -->
+              <h1 class="uk-heading-primary uk-text-<?php echo $settings['title_transformation'];?>">
+                <?php echo $contents['title'];?>
+              </h1>
+            <?php endif; ?>
 
-              <div class="section-heading uk-text-<?php echo $title_alignment;?>" <?php echo $title_animation;?>>
-                    <?php if($contents['title']):?>
-                      <!-- Section Title -->
-                      <h1 class="uk-heading-primary uk-text-<?php echo $settings['title_transformation'];?>">
-                        <?php echo $contents['title'];?>
-                      </h1>
-                    <?php endif; ?>
-
-                    <?php if($contents['description']):?>
-                        <div class="uk-text-lead"><?php echo $contents['description']?></div>
-                    <?php endif; ?>
-              </div> <!-- Section heading -->
+            <?php if($contents['description']):?>
+                <div class="uk-text-lead"><?php echo $contents['description']?></div>
+            <?php endif; ?>
+        </div> <!-- Section heading -->
 
 
-              <div class="uk-grid-medium" uk-grid>
-                  <?php foreach($contents['pricings'] as $k=>$pricing): ?>
-
-                      <div class="uk-width-1-<?php echo $settings['items_columns'];?>@m">
-                          <div class="price-table uk-text-center <?php echo $pricing['featured'] ? 'featured': ''?>">
-                                <span class="pricing-title"><?php echo $pricing['title'];?></span>
-                                <div class="value">
-                                      <span><?php echo $pricing['money'];?></span>
-                                      <span><?php echo $pricing['price'];?></span><br>
-                                      <span><?php echo $pricing['period'];?></span>
-                                </div>
-                                <ul>
-                                    <?php foreach($pricing['features'] as $feature): ?>
-                                    <li><?php echo $feature;?></li>
-                                    <?php endforeach; ?>
-                                    <li><?php echo op_link($pricing['link'], 'uk-width-1-1');?></li>
-                                </ul>
-                          </div>
-                      </div>
-
-                  <?php endforeach; ?>
-              </div>
-          </div> <!-- uk-grid -->
-      </div> <!-- uk-container -->
+        <div class="uk-grid-medium" uk-grid>
+            <?php foreach($contents['pricings'] as $k=>$pricing): ?>
+            <div class="uk-width-1-<?php echo $settings['items_columns'];?>@m">
+                <div class="price-table uk-text-center <?php echo $pricing['featured'] ? 'featured': ''?>">
+                    <span class="pricing-title"><?php echo $pricing['title'];?></span>
+                    <div class="value">
+                          <span><?php echo $pricing['money'];?></span>
+                          <span><?php echo $pricing['price'];?></span><br>
+                          <span><?php echo $pricing['period'];?></span>
+                    </div>
+                    <ul>
+                        <?php foreach($pricing['features'] as $feature): ?>
+                        <li><?php echo $feature;?></li>
+                        <?php endforeach; ?>
+                        <li><?php echo op_link($pricing['link'], 'uk-width-1-1');?></li>
+                    </ul>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div> <!-- uk-grid -->
 </section> <!-- end-section -->
