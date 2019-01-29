0cd17c155249a55cd8d25f1937ccacd2868991cf
Abdullah Al Mamun <mamunhai@gmail.com>
2019-01-29 10:11:00 +0600

    Portfolio 1 recheck the codes

--- blocks/portfolios/portfolio-1/view.php
+++ blocks/portfolios/portfolio-1/view.php
@@ -12,7 +12,7 @@
 <section id="<?php echo $id; ?>" class="uk-section portfolios portfolio-1">
 	<div class="uk-container">
 
-		<div class="section-heading uk-text-<?php echo $title_alignment;?>" <?php echo $title_animation;?>>	
+		<div class="section-heading uk-margin-large-bottom uk-text-<?php echo $title_alignment;?>" <?php echo $title_animation;?>>	
 			<?php if($contents['title']):?>
 				<!-- Section Title -->
 				<h1 class="uk-heading-primary uk-text-<?php echo $settings['title_transformation'];?>">
