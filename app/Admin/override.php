30f5b08755550faa8d1c29d7aa8e9b2511a394c6
Parvez Akther <parvez@themexpert.com>
2019-03-13 12:27:38 +0600

    New: Onepager default page template added that support theme header/footer Update: Renamed onepager to Onepager Canvas to avoid conflict

--- app/Admin/override.php
+++ app/Admin/override.php
@@ -8,9 +8,10 @@ function op_edit_with_onepager_link_in_page_row( $actions, $post ) {
     // URL
     $page_permalink = onepager_get_edit_mode_url(get_permalink($post->ID)); 
     // Page template
-    $onepage_template = get_post_meta( $post->ID, '_wp_page_template', true );
+    $template = get_post_meta( $post->ID, '_wp_page_template', true );
 
-    if($onepage_template == 'onepage.php' && (current_user_can('editor') || current_user_can('administrator')) ) {
+    if( (strpos($template, 'onepage') !== false) && 
+        (current_user_can('editor') || current_user_can('administrator')) ) {
       $actions['edit_with_onepager'] = sprintf(
         '<a href="%1$s">%2$s</a>',
         $page_permalink,
@@ -23,9 +24,10 @@ function op_edit_with_onepager_link_in_page_row( $actions, $post ) {
 add_filter( 'display_post_states', 'op_append_onepager_in_page_title', 10, 2 );
 
 function op_append_onepager_in_page_title( $post_states, $post ) {
-  $onepage_template = get_post_meta( $post->ID, '_wp_page_template', true );
+  $template = get_post_meta( $post->ID, '_wp_page_template', true );
 
-  if($onepage_template == 'onepage.php' && (current_user_can('editor') || current_user_can('administrator')) ) {
+  if( (strpos($template, 'onepage') !== false) && 
+      (current_user_can('editor') || current_user_can('administrator')) ) {
     $post_states['onepager'] = __( 'Onepager', 'txop' );
   }
   return $post_states;
