f0f11ceaea9493f35fffb325732baf2ce8185f4b
Parvez Akther <parvez@themexpert.com>
2019-03-22 11:53:45 +0600

    Upgrade to PRO version notice added in dashboard

--- app/Admin/views/dashboard.php
+++ app/Admin/views/dashboard.php
@@ -12,7 +12,42 @@ function op_get_html_group_class($groups)
 }
 ?>
 <div class="wrap" uk-filter="target: .layout-filter">
-  <h1 class="uk-title">Dashboard</h1>
+  <h1 class="uk-title"><?php _e('Dashboard', 'onepager')?></h1>
+
+  <?php if(!did_action('onepager_pro_loaded')):?>
+    <div class="uk-card uk-card-small uk-card-secondary uk-card-body uk-margin uk-margin-medium-top uk-margin-medium-bottom">
+      <div class="uk-grid uk-grid-small uk-flex-middle">
+        <div class="uk-width-2-3">
+          <div class="uk-grid uk-grid-small">
+            <div class="uk-width-1-6 uk-first-column">
+              <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                width="60px" height="40px" viewBox="251 356 110 80" enable-background="new 251 356 110 80" xml:space="preserve" fill="#fff">
+                <g>
+                  <path d="M291.9,433.6l-25.1-67.7c0.3-2.5,2.4-3,4.6-3.1c2.2-0.2,49-1.3,49-1.3c5,0,6.2,1.3,7.5,3.3c1.3,2,17.8,30.6,17.8,30.6
+                    c1.5,3.1-1.4,3.6-3.5,3.8c-3.1,0.4-48.8,4.6-48.8,4.6l4.6,12.1l54.9-5.6c8.8-2.6,8-9,6.2-12.3c-1.8-3.2-25-37.8-25-37.8
+                    c-2.7-3.6-7.7-4.3-11.7-4.3c-4-0.1-57.5-0.3-57.5-0.3c-12.8,0.1-14.5,4.6-14.6,8.3c-0.1,3.6,21.1,71.7,21.1,71.7L291.9,433.6z"/>
+                  <path d="M316.5,368.7L316.5,368.7c-0.2,0-25.3,1.3-26.2,1.3c-1,0.1-1.1,0.3-1.1,0.3s-0.1,0.2,0.1,0.7l6.6,15.9
+                    c0,0.1,0.7,1.6,2.5,1.5c7.2-0.3,27.5-1.9,28.6-2.1c0.8-0.1,0.9-0.3,0.9-0.3s0-0.2-0.3-0.7l0,0c-2.3-4.1-8.2-14.9-8.7-15.5
+                    C318.6,369.1,318.3,368.7,316.5,368.7z"/>
+                  <path d="M325.8,389.3c0,0-0.2-0.4-0.5-1c-5.1,0.5-21,1.7-27.2,2c-0.1,0-0.1,0-0.2,0c-2.7,0-3.7-2.3-3.7-2.4l-6-14.2
+                    c-0.2,0-0.4,0-0.4,0c-2,0.1-2.1,1-1.7,2l6.3,15.2c0,0.1,1.1,2.6,4,2.6c0.1,0,0.1,0,0.2,0l28-2.1C325.4,391.4,326.9,391,325.8,389.3
+                    z"/>
+                </g>
+              </svg>
+            </div>
+            <div class="uk-width-5-6">
+              <h5 class="uk-margin-remove-bottom"><?php _e('Unlock More Layouts', 'onepager')?></h5>
+              <p class="uk-margin-remove-top">Upgrade to PRO version and unlock more layouts, blocks and features you could imagine.</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="uk-width-1-3">
+          <a href="https://themesgrove.com/wp-onepager/?utm_source=wp-admin&utm_medium=dashboard&utm_campaign=wponepager-gopro" target="_blank" class="uk-button uk-button-danger">Explore Onepager Pro</a>
+        </div>
+      </div>
+    </div>
+  <?php endif;?>
   
   <ul class="uk-subnav uk-subnav-pill" uk-margin>
     <li class="uk-active" uk-filter-control><a href="#">All</a></li>
