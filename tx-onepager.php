a6fdb1a5e1b7d5d4768c98c79a87452b82687041
anamwp <anam@themexpert.com>
2020-03-22 16:04:35 +0600

    version updated to 2.3.0

--- tx-onepager.php
+++ tx-onepager.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WPOnepager
  * Plugin URI:        https://themesgrove.com/wp-onepager
  * Description:       The best and most easiest, beginner friendly landing page builder. Create one page website faster than ever.
- * Version:           2.2.9
+ * Version:           2.3.0
  * Author:            Themesgrove
  * Author URI:        https://themesgrove.com/
  * License:           GPL-2.0+
@@ -18,7 +18,7 @@ if (!defined('WPINC')) {
 }
 
 if (!defined('ONEPAGER_VERSION')) {
-    define('ONEPAGER_VERSION', '2.2.9');
+    define('ONEPAGER_VERSION', '2.3.0');
 }
 
 if (!defined('ONEPAGER_PHP_VERSION')) {
