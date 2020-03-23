1cf22b7fff414ac8156c1886cddc202dc9a900b8
anamwp <anam@themexpert.com>
2020-03-23 16:19:48 +0600

    new badge added in onepager dashboard inside layout pack

--- app/Admin/views/dashboard.php
+++ app/Admin/views/dashboard.php
@@ -24,7 +24,7 @@ function op_get_html_group_class( $groups ) {
 	);
 }
 ?>
-<div class="wrap" uk-filter="target: .layout-filter">
+<div class="wrap txop-admin txop-dashboard" uk-filter="target: .layout-filter">
   <h1 class="uk-title"><?php _e( 'Dashboard', 'onepager' ); ?></h1>
 
 	<?php if ( ! did_action( 'onepager_pro_loaded' ) ) : ?>
@@ -75,8 +75,12 @@ function op_get_html_group_class( $groups ) {
 
 	<div class="layout-filter uk-child-width-1-2@s uk-child-width-1-4@m" uk-grid>
 		<?php foreach ( $layouts as $layout ) : ?>
+		<?php $type = array_get($layout, 'type') ? $layout['type'] : '';?>
 		<div data-group="<?php echo op_get_html_group_class( $layout['group'] ); ?>">
 			<div class="uk-card uk-card-default uk-transition-toggle" tabindex="0" >
+					<?php if($type):?>
+						<span class="txop-new-badge"><?php echo $type; ?></span>
+					<?php endif;?>
 					<div class="uk-card-media-top uk-inline uk-height-medium" uk-overflow-auto>
 						<img data-src="<?php echo $layout['screenshot']; ?>" alt="<?php echo $layout['name']; ?>" uk-img >
 					</div>
